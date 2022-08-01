592e77f139406d4ff4f665526b44f94633cda346
xiaofei <alibaba22001@163.com>
2022-08-01 09:16:56 +0800

    update

---
+++ apps/adc_value_helper.h
@@ -0,0 +1,26 @@
+
+
+/*================================================================
+ *   
+ *   
+ *   文件名称：adc_value_helper.h
+ *   创 建 者：肖飞
+ *   创建日期：2022年08月01日 星期一 09时15分08秒
+ *   修改日期：2022年08月01日 星期一 09时15分25秒
+ *   描    述：
+ *
+ *================================================================*/
+#ifndef _ADC_VALUE_HELPER_H
+#define _ADC_VALUE_HELPER_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "app_platform.h"
+#include "cmsis_os.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif //_ADC_VALUE_HELPER_H
--- apps/channels_config.c
+++ apps/channels_config.c
@@ -6,14 +6,13 @@
  *   文件名称：channels_config.c
  *   创 建 者：肖飞
  *   创建日期：2021年01月18日 星期一 09时26分44秒
- *   修改日期：2022年08月01日 星期一 09时05分49秒
+ *   修改日期：2022年08月01日 星期一 09时13分07秒
  *   描    述：
  *
  *================================================================*/
 #include "channels_config.h"
 #include "os_utils.h"
 #include "power_modules.h"
-#include "adc_value_type.h"
 
 extern CAN_HandleTypeDef hcan1;//comm
 extern CAN_HandleTypeDef hcan2;//charger1
