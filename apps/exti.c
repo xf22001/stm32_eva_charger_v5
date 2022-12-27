cce967e1faf008a746cf04c4094395f69eda189b
xiaofei <alibaba22001@163.com>
2022-12-27 11:44:38 +0800

    update

--- apps/exti.c
+++ apps/exti.c
@@ -6,7 +6,7 @@
  *   文件名称：exti.c
  *   创 建 者：肖飞
  *   创建日期：2021年01月26日 星期二 14时22分36秒
- *   修改日期：2022年01月23日 星期日 14时32分12秒
+ *   修改日期：2022年12月27日 星期二 11时43分58秒
  *   描    述：
  *
  *================================================================*/
@@ -19,11 +19,7 @@ extern SPI_HandleTypeDef hspi3;
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	switch(GPIO_Pin) {
-		//case isr_pin_Pin: {
-		//	wiz_isr();
-		//}
-		//break;
-		case GPIO_PIN_8: {
+		case SPICAN_INT1_Pin: {
 			spi_can_isr(&hspi3);
 		}
 		break;
