f91a0b58f96e7259b1accd3798ad2a4cd0777fd2
xiaofei <alibaba22001@163.com>
2023-01-03 10:08:12 +0800

    update

--- apps/exti.c
+++ apps/exti.c
@@ -6,12 +6,13 @@
  *   文件名称：exti.c
  *   创 建 者：肖飞
  *   创建日期：2021年01月26日 星期二 14时22分36秒
- *   修改日期：2022年12月27日 星期二 11时43分58秒
+ *   修改日期：2023年01月03日 星期二 10时07分49秒
  *   描    述：
  *
  *================================================================*/
 #include "app_platform.h"
 #include "cmsis_os.h"
+#include "main.h"
 
 void spi_can_isr(void *hcan);
 extern SPI_HandleTypeDef hspi3;
